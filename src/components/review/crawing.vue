d0a4f8fc3aabd0e1da630bcd0ae9a17aced2e782
DESKTOP-3VJ4QNV\admin <2785021810@qq.com>
2023-02-11 17:53:16 +0800

    添加成交额排行

--- src/components/review/crawing.vue
+++ src/components/review/crawing.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-row :gutter="20">
-            <el-col :span="8">
+            <el-col :span="6">
                 <div style="text-align: center;">
                     <el-row>
                         <el-col>
@@ -51,7 +51,50 @@
                     </el-row>
                 </div>
             </el-col>
-            <el-col :span="16">
+            <el-col :span="6">
+                <div style="text-align: center;">
+                    <el-row>
+                        <el-col>
+                            <h2>成交额前10</h2>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="12">
+                            <span><a href="https://www.iwencai.com/unifiedwap/result?w=今日成交额从高到低；"
+                                    target="_blank">问财链接</a></span>
+                        </el-col>
+                        <el-col :span="12">
+                            <span><a href="http://data.10jqka.com.cn/market/longhu/" target="_blank">同花顺龙虎榜</a></span>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="24">
+                            <el-card class="box-card">
+                                <el-table :data="index" border style="width: 100%">
+                                    <el-table-column prop="stockName" label="名称">
+                                        <span slot-scope="scope">
+                                            <a :href=scope.row.xueQiuLink target="_blank" style="margin-left: 6px">{{
+                                                scope.row.stockName
+                                            }}</a>
+                                        </span>
+                                    </el-table-column>
+                                    <el-table-column prop="trend" label="涨幅">
+                                        <span slot-scope="scope"
+                                            :class="(scope.row.trend.slice(0, 1) == '-') ? 'green' : 'red'">
+                                            {{ scope.row.trend }}
+                                        </span>
+                                    </el-table-column>
+                                    <el-table-column prop="nowPrice" label="现价">
+                                    </el-table-column>
+                                    <el-table-column prop="turnover" label="成交额">
+                                    </el-table-column>
+                                </el-table>
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </div>
+            </el-col>
+            <el-col :span="12">
                 <div style="text-align: center;">
                     <el-row>
                         <el-col :span="24">
