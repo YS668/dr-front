53b2c416125cfff389bc41a23a60519816ced591
DESKTOP-3VJ4QNV\admin <2785021810@qq.com>
2023-02-11 17:12:41 +0800

    编辑器界面完善字体

--- src/components/Editor/Index.vue
+++ src/components/Editor/Index.vue
@@ -37,7 +37,7 @@ export default {
                 skin_url: '/tinymce/skins/ui/oxide',
                 height: 380, //编辑器高度
                 width: 1000, //宽度
-                menubar:'', //菜单栏
+                menubar: '', //菜单栏
                 browser_spellcheck: true, // 拼写检查
                 // elementpath: false, //禁用编辑器底部的状态栏
                 // statusbar: false, // 隐藏编辑器底部的状态栏
@@ -51,9 +51,16 @@ export default {
                 //toolbar: "fontselect fontsizeselect link lineheight forecolor backcolor bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | image quicklink h2 h3 blockquote table numlist bullist preview fullscreen", //工具栏
                 fontsize_formats: "12px 14px 16px 18px 24px 36px 48px 56px 72px",
                 font_formats: "微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;",
+                setup: function (editor) {
+                    editor.on('init', function (e) {
+                        this.getBody().style.fontSize = '14px';
+                    });
+                },
             },
-            content:this.value
+            content: this.value,
         }
+    },
+    methods: {
     },
     created() {
         tinymce.init({});
@@ -64,7 +71,7 @@ export default {
         },
         content(newValue) {
             this.$emit("input", newValue);
-        }
+        },
     },
     components: {
         Editor
