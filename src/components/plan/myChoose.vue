f1e1645a80f5435575598ad2885bc5492c48fd57
DESKTOP-3VJ4QNV\admin <2785021810@qq.com>
2023-02-20 20:19:46 +0800

    自选股界面完善

--- src/components/plan/myChoose.vue
+++ src/components/plan/myChoose.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
         <div style="margin-bottom: 5px;">
-            <el-input v-model="stockName" placeholder="请输入名称" suffix-icon="el-icon-search" style="width: 200px;"
+            <el-input clearable v-model="stockName" placeholder="请输入名称" suffix-icon="el-icon-search" style="width: 200px;"
                 @keyup.enter.native="loadPost" max-height="800"></el-input>
             <el-select v-model="groupName" placeholder="选择分组">
                 <el-option v-for="(item) in groups" :key="item" :label="item" :value="item"></el-option>
             </el-select>
             <el-button type="warning" @click="restCondition">重置</el-button>
             <el-button type="primary" style="margin-left: 5px;" @click="loadPost">查询</el-button>
-            <el-button type="warning" @click="del">删除</el-button>
+            <el-button type="warning" @click="stockDel">删除</el-button>
             <el-button type="primary" @click="changeGroup">换组</el-button>
         </div>
         <el-table ref="multipleTable" @selection-change="handleSelectionChange" :data="tableData"
-            :header-cell-style="rowClass" border style="width: 100%" highlight-current-row max-height="800">
+            :header-cell-style="rowClass" border style="width: 100%" highlight-current-row max-height="700">
             <el-table-column type="selection" width="55">
             </el-table-column>
             <el-table-column fixed prop="stockName" width="110" label="名称" align="left">
@@ -45,12 +45,11 @@
                     <el-tag effect="dark">{{ rdidFormat(scope.row.rdid) }}</el-tag>
                 </span>
             </el-table-column>
-            <el-table-column sortable prop="groupName" label="分组" align="left">
+            <el-table-column prop="groupName" label="分组" align="left">
             </el-table-column>
             <el-table-column prop="note" label="备注">
                 <template slot-scope="scope">
-                    <el-button size="small" type="success"
-                        @click="getNote(scope.row.note, scope.row.stockName)">查看</el-button>
+                    <el-button size="small" type="success" @click="getNote(scope.row)">查看</el-button>
                 </template>
             </el-table-column>
             <el-table-column label="链接" align="left">
@@ -64,15 +63,26 @@
         </el-table>
         <nav style="text-align: center"> <!-- 分页居中放置-->
             <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pageNum"
-            :page-sizes="[999, 30, 10]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper"
-            :total="total">
-        </el-pagination>
+                :page-sizes="[999, 30, 10]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+            </el-pagination>
         </nav>
         <nav style="text-align: center"> <!-- 分页居中放置-->
-            <chooseStockVue @func="getGroupsAndUser"></chooseStockVue>
+            <chooseStockVue @func="getGroupsAndUser" @addStockSus="loadPost"></chooseStockVue>
         </nav>
-        <el-dialog :visible.sync="dialogVisible" width="50%" center :title="tit">
-            <div v-html="strHtml" class="ql-editor">
+        <el-dialog :visible.sync="noteVisible" width="50%" center :title="stockNameTit" style="margin-bottom: 5px;">
+            <div style="margin:10px;border:0;padding:0;text-align: center">
+                <el-input clearable type="textarea" style="width: 50%;margin: 10px;" maxlength="150" autosize
+                    v-model="noteStr" placeholder="暂时无备注，输入内容"></el-input>
+                <el-button type="primary" @click="updateNote">确认</el-button>
+            </div>
+        </el-dialog>
+        <el-dialog :visible.sync="groupVisible" width="28%" center title="请选择你要更改的分组并确认">
+            <div style="margin:10px;border:0;padding:0;text-align: center">
+                <el-select v-model="newGroup" placeholder="选择分组">
+                    <el-option v-for="(item) in groups" :key="item" :label="item" :value="item"></el-option>
+                </el-select>
+                <el-button type="primary" @click="startGroup">确认</el-button>
             </div>
         </el-dialog>
     </div>
@@ -85,30 +95,42 @@ export default {
     name: "MyChoose",
     data() {
         return {
+            //选中的
             multipleTable: [],
             tableData: [],
-            dialogVisible: false,
-            strHtml: '',
-            tit: '',
+            noteVisible: false,
+            groupVisible: false,
+            //备注
+            noteStr: '',
+            //备注对话框标题
+            stockNameTit: '',
+            //备注对话的股票分组
+            noteSrockGroup: '',
+            //交换的分组
+            newGroup: '',
+            //条件
             stockName: '',
+            //条件
             groupName: '',
             pageSize: 999,
             pageNum: 1,
             total: 0,
+            //用户
             user: '',
+            //分时图数组
             gifList: [],
-            groups: ['北京', '上海'],
+            //分组数组
+            groups: [],
+            //删除，换组条件
+            delOrChange: [],
         }
     },
     methods: {
         //接受子组件的消息
-        getGroupsAndUser(val, val2) {
+        getGroupsAndUser(val) {
             this.groups = val
-            this.user = val2
-            console.log(val2)
         },
         loadPost() {
-            console.log(this.user)
             this.$axios.post('/my/stock/page', {
                 pageSize: this.pageSize,
                 pageNum: this.pageNum,
@@ -137,24 +159,124 @@ export default {
             this.multipleTable = val
             console.log(this.multipleTable)
         },
+        changeOrdel(){
+            this.delOrChange = this.multipleTable.map(function (item, index) {
+                var temp = {
+                    uid: item.uid,
+                    stockName: item.stockName,
+                    groupName: item.groupName
+                }
+                return temp
+            })
+        },
         //换组
         changeGroup() {
-
+            if (this.multipleTable.length == 0) {
+                this.$notify.error({
+                    title: '失败',
+                    message: '请先选择对应的股票',
+                    offset: 100
+                });
+                return
+            }
+            this.groupVisible = true;
         },
-        //删除
-        del() {
-
+        //确认换组
+        startGroup(val) {
+            //转换
+            this.changeOrdel()
+            if (this.newGroup == '') {
+                this.$notify.error({
+                    title: '失败',
+                    message: '请先选择对应的分组',
+                    offset: 100
+                });
+                return
+            }
+            //转换
+            this.delOrChange.forEach(item => item.newGroup = this.newGroup )
+            this.$axios.post('/my/stock/change/group', this.delOrChange).then(res => {
+                if (res.code == 200) {
+                    this.$message({
+                        message: '操作成功！',
+                        type: 'success'
+                    });
+                    //清空
+                    this.newGroup == ''
+                    //关闭对话框
+                    this.groupVisible = false;
+                    //重新加载
+                    this.loadPost();
+                } else {
+                    this.$message({
+                        message: '操作失败！',
+                        type: 'error'
+                    });
+                }
+            })
+        },
+        //删除自选股
+        stockDel() {
+            if (this.multipleTable.length == 0) {
+                this.$notify.error({
+                    title: '失败',
+                    message: '请先选择对应的股票',
+                    offset: 100
+                });
+                return
+            }
+            //转换
+            this.changeOrdel()
+            this.$axios.post('/my/stock/del', this.delOrChange).then(res => {
+                if (res.code == 200) {
+                    this.$message({
+                        message: '操作成功！',
+                        type: 'success'
+                    });
+                } else {
+                    this.$message({
+                        message: '操作失败！',
+                        type: 'error'
+                    });
+                }
+            })
         },
         //备注
-        getNote(val, val2) {
-            this.strHtml = val
-            this.dialogVisible = true
-            this.tit = val2
+        getNote(val) {
+            this.noteStr = val.note
+            this.noteVisible = true
+            this.stockNameTit = val.stockName
+            this.noteSrockGroup = val.groupName
+        },
+        //修改备注
+        updateNote() {
+            this.$axios.post('/my/stock/update/note', {
+                uid: this.user.uid,
+                groupName: this.noteSrockGroup,
+                stockName: this.stockNameTit,
+                note: this.noteStr,
+            }).then(res => {
+                if (res.code == 200) {
+                    this.$message({
+                        message: '操作成功！',
+                        type: 'success'
+                    });
+                    //加载数据
+                    this.loadPost()
+                    //隐藏对话框
+                    this.noteVisible = false
+                } else {
+                    this.$message({
+                        message: '操作失败！',
+                        type: 'error'
+                    });
+                }
+            })
         },
         //清空条件
         restCondition() {
-            this.group = ''
-            this.name = ''
+            this.groupName = ''
+            this.stockName = ''
         },
         handleSizeChange(val) {
             console.log(`每页 ${val} 条`);
@@ -186,7 +308,18 @@ export default {
         },
     },
     mounted() {
+        this.user = JSON.parse(sessionStorage.getItem('CurUser'))
         this.loadPost();
+        //多次执行定时器
+        this.Timer = setInterval(() => {
+            this.loadPost()
+            //30秒，毫秒为单位    
+        }, 1 * 30 * 1000)
+    },
+    beforeDestroy() {
+        //清楚一次性定时器
+        clearInterval(this.Timer)
+        console.log('清除定时器timeout')
     },
     components: {
         chooseStockVue
@@ -194,6 +327,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/utils/chooseStock.vue
+++ src/components/utils/chooseStock.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <el-button type="success" @click="stockVisible = true">添加自选股</el-button>
-        <el-button type="primary" @click="groupVisible = true">新建分组</el-button>
-        <el-dialog :visible.sync="stockVisible" width="50%" center >
+        <el-button type="primary" @click="groupVisible = true">分组管理</el-button>
+        <el-dialog :visible.sync="stockVisible" width="50%" center title="添加自选股" >
             <el-form ref="form" :model="form" label-width="80px" label-position="right">
                 <el-form-item label="股票名称">
-                    <el-input v-model="form.stockName" placeholder="请输入股票名称"></el-input>
+                    <el-input clearable maxlength="20" v-model="form.stockName" placeholder="请输入股票名称"></el-input>
                 </el-form-item>
                 <el-form-item label="分组">
                     <el-select v-model="form.groupName" placeholder="请选择分组" style="margin: 10px;">
@@ -23,16 +23,17 @@
             </el-form>
         </el-dialog>
 
-        <el-dialog :visible.sync="groupVisible" width="30%" center >
-            <el-form ref="form"  label-width="80px" label-position="right">
-                <el-form-item label="分组名称">
-                    <el-input v-model="addGroupName" placeholder="请输入分组名称"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="addGroup">确认</el-button>
-                    <el-button @click="groupVisible = false">取消</el-button>
-                </el-form-item>
-            </el-form>
+        <el-dialog :visible.sync="groupVisible" width="38%" center  title="分组管理">
+                <div style="margin:10px;border:0;padding:0;text-align: center">
+                    <el-input clearable v-model="addGroupName"  style="width: 217px;margin: 10px;" placeholder="请输入新建分组名称"></el-input>
+                    <el-button type="primary" @click="addGroup">新建</el-button>
+                </div>
+                <div style="margin:10px;border:0;padding:0;text-align: center">
+                    <el-select v-model="delGroupName" placeholder="选择分组"  style="width: 217px;margin: 10px;">
+                        <el-option v-for="(item) in groups" :key="item" :label="item" :value="item"></el-option>
+                    </el-select>
+                    <el-button type="primary" @click="delGroup">删除</el-button>
+                </div>
         </el-dialog>
     </div>
 </template>
@@ -49,25 +50,22 @@ export default {
                 groupName: '',
                 note: '',
             },
-            addGroupName:'',
-            user:'',
+            addGroupName: '',
+            user: '',
+            delGroupName:'',
             groups: [],
             stockVisible: false,
             groupVisible: false,
         }
     },
     methods: {
-        loadUid(){
-            this.user = JSON.parse(sessionStorage.getItem('CurUser'))
-        },
         loadGroup() {
-            this.$axios.get('/stock/group/all/'+this.user.uid).then(res => {
+            this.$axios.get('/stock/group/all/' + this.user.uid).then(res => {
                 if (res.code == 200) {
                     this.$message({
                         message: '操作成功！',
                         type: 'success'
                     });
-                    console.log(res.data)
                     this.groups = res.data
                 } else {
                     this.$message({
@@ -79,7 +77,7 @@ export default {
 
         },
         addStock() {
-            this.$axios.post('/my/stock/save',{
+            this.$axios.post('/my/stock/save', {
                 uid: this.user.uid,
                 groupName: this.form.groupName,
                 stockname: this.form.stockName,
@@ -90,6 +88,12 @@ export default {
                         message: '操作成功！',
                         type: 'success'
                     });
+                    //清空表单
+                    this.resetStockDialog()
+                    //抛出事件，父组件监听
+                    this.$emit('addStockSus');
+                    //隐藏组件
+                    stockVisible = false
                 } else {
                     this.$message({
                         message: '操作失败！',
@@ -99,7 +103,7 @@ export default {
             })
         },
         addGroup() {
-            this.$axios.post('/stock/group/save/',{
+            this.$axios.post('/stock/group/save/', {
                 uid: this.user.uid,
                 groupname: this.addGroupName
             }).then(res => {
@@ -108,6 +112,9 @@ export default {
                         message: '操作成功！',
                         type: 'success'
                     });
+                    //清空表单
+                    this.addGroupName = ''
+                    //加载分组
                     this.loadGroup()
                 } else {
                     this.$message({
@@ -116,13 +123,50 @@ export default {
                     });
                 }
             })
-        }
+        },
+        delGroup(){
+            this.$axios.post('/stock/group/del/'+this.user.uid+'/'+this.delGroupName).then(res => {
+                if (res.code == 200) {
+                    this.$message({
+                        message: '操作成功！',
+                        type: 'success'
+                    });
+                    //清空表单
+                    this.delGroupName = ''
+                    //加载分组
+                    this.loadGroup()
+                } else {
+                    this.$message({
+                        message: '操作失败！',
+                        type: 'error'
+                    });
+                }
+            })
+        },
+        resetStockDialog() {
+            this.form.groupName = ''
+            this.form.stockName = ''
+            this.form.note = ''
+        },
     },
     created() {
-        this.loadUid()
+        this.user = JSON.parse(sessionStorage.getItem('CurUser'))
         this.loadGroup();
+    },
+    watch: {
+        groups: {
+            handler(newVal, oldVal) {
+                this.groups = newVal
+                this.$emit('func', newVal, this.user)
+            },
+            deep: true
+        }
+    },
+    mounted() {
         //func: 是父组件指定的传数据绑定的函数，this.groups:子组件给父组件传递的数据
-        this.$emit('func',this.groups,this.user)
+        console.log("子组件")
+        console.log(this.groups)
+
     },
     components: {
 
@@ -130,6 +174,6 @@ export default {
 }
 </script>
 
-<style>
 
+<style>
 </style>
