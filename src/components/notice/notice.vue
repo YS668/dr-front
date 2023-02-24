b06963d9cb4e88af52ccb117c1651b241a843fed
DESKTOP-3VJ4QNV\admin <2785021810@qq.com>
2023-02-24 17:13:39 +0800

    公告界面

--- src/components/notice/notice.vue
+++ src/components/notice/notice.vue
@@ -1,26 +1,95 @@
 <!--公告界面-->
 <template>
-    <div>
-      <span>公告界面</span>
-    
+  <div>
+    <div style="text-align: center;">
+      <DateUtils></DateUtils>
     </div>
-  </template>
+    <div v-loading="loading" element-loading-text="拼命加载中" element-loading-spinner="el-icon-loading" v-for="item in notice" :key="item.topic" style="margin-top: 20px;">
+      <textRoll  :value="item.content" @click.native="show(item)"></textRoll>
+    </div>
+    <el-dialog  :visible.sync="dialogVisible" width="30%" center  :title="tit" >
+      <span>{{ str }}</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
   
-  <script type="text/javascript">
+<script type="text/javascript">
+import DateUtils from "../utils/DateUtils";
+import textRoll from '../utils/textRoll';
 
-  export default {
-    name: "Notice",
-    data() {
-      return {
-  
-      }
+export default {
+  name: "Notice",
+  data() {
+    return {
+      notice: [
+        {
+          topic: '1',
+          content: '1发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代',
+        },
+        {
+          topic: '2',
+          content: '2发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代',
+        },
+        {
+          topic: '3',
+          content: '3发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代',
+        },
+        {
+          topic: '4',
+          content: '4发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代',
+        },
+        {
+          topic: '5',
+          content: '5发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代',
+        },
+        {
+          topic: '6',
+          content: '6发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代发哈设计开发你回去设计开发首府拉萨就发生了发起设立开发商的发哈十六分爱上了开发阿斯弗时代',
+        },
+      ],
+      loading: true,
+      dialogVisible:false,
+      tit:'',
+      str:''
+    }
+  },
+  methods: {
+    //加载公告
+    loadPost() {
+      this.$axios.get('').then(res => {
+        if (res.code == 200) {
+          this.$message({
+            message: '操作成功！',
+            type: 'success'
+          });
+          this.loading = false
+        } else {
+          this.$message({
+            message: '操作失败！',
+            type: 'error'
+          });
+        }
+      })
     },
-    components: {
-    },
+    show(val) {
+      this.tit = val.topic
+      this.str = val.content
+      this.dialogVisible = true
+    }
+  },
+  created() {
+    this.loadPost()
+  },
+  components: {
+    DateUtils,
+    textRoll
+  },
 
-  }
-  </script>
+}
+</script>
   
-  <style lang="less" scoped>
-  </style>
+<style lang="less" scoped></style>
   
---
+++ src/components/utils/textRoll.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- 跑马灯组件 -->
+    <div style="top: 10px;">
+        <el-card class="box-card" style="text-align: center;">
+            <i class="el-icon-message-solid"></i>
+            {{ text.substring(0, 80) }}
+        </el-card>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'TextRoll',
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+    },
+    data() {
+        return {
+            timer: null,
+            text: this.value,
+        }
+    },
+    created() {
+        //多次执行定时器
+        this.Timer = setInterval(() => {
+            this.lang()
+        }, 500)
+    },
+    watch: {
+        value(newValue) {
+            this.text = newValue;
+        },
+    },
+    methods: {
+        lang() {
+            var start = this.text.slice(0, 1)
+            var end = this.text.substring(1)
+            this.text = end + start
+        }
+    },
+    beforeDestroy() {
+        //清楚定时器
+        clearInterval(this.Timer)
+        console.log('清除定时器timeout')
+    },
+}
+</script>
+<style scoped lang='less'>
+</style>
+  
+  
